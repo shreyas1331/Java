423ae38bffb6e59acc266011af293c781ca8e1b8
Divyam <96142719+Crag73@users.noreply.github.com>
2023-05-20 23:44:00 +0530

    Update Insert_Begin.java
    Patch 1.0

--- Linked_List2/Insert_Begin.java
+++ Linked_List2/Insert_Begin.java
@@ -1,28 +1,24 @@
-package Linked_List2;
-
 public class Insert_Begin {
-	public class Node{
+	public static class Node{
 		int data;
 		Node next;
 		Node(int x){
 			this.data=x;
 			next=null;
-		}	
-	}
-	class Test{
-		 static Node insertBegin(Node head,int x) {
+		}
+	}	
+		static Node insertBegin(Node head,int x) {
 			Node temp=new Node(x);
 			temp.next=head;
+			head=temp;
 			return temp;
 		}
 		public static void main(String[] args) {
 			Node head=null;
-			head=insertBegin(head, 30);
-			head=insertBegin(head, 20);
-			head=insertBegin(head, 10);
+			head=insertBegin(head,30);
+			head=insertBegin(head,20);
+			head=insertBegin(head,10);
 		}
-	}
 
 }
-
 
