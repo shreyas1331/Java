fb189d8d3101396fa1159c29d97770e3cf396f11
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-05-20 22:55:34 +0530

    Linked List

---
+++ Linked_List2/Insert_Begin.java
@@ -0,0 +1,28 @@
+package Linked_List2;
+
+public class Insert_Begin {
+	public class Node{
+		int data;
+		Node next;
+		Node(int x){
+			this.data=x;
+			next=null;
+		}	
+	}
+	class Test{
+		 static Node insertBegin(Node head,int x) {
+			Node temp=new Node(x);
+			temp.next=head;
+			return temp;
+		}
+		public static void main(String[] args) {
+			Node head=null;
+			head=insertBegin(head, 30);
+			head=insertBegin(head, 20);
+			head=insertBegin(head, 10);
+		}
+	}
+
+}
+
+
---
+++ Linked_List2/linked_list.java
@@ -0,0 +1,26 @@
+package Linked_List2;
+//Recursive Traversal Of Singly Linked List
+class Node {
+    int data;
+    Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+}
+public class linked_list {
+    public static void printList(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.println(head.data);
+        printList(head.next);
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1, new Node(2, new Node(3, null)));
+        printList(head);
+    }
+}
+
