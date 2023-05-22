89d1f703cd77de2a401e1678b53b1ccd2178e4ee
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-05-22 23:04:59 +0530

    Abstraction

--- Abstraction/Hello.java
+++ Abstraction/Hello.java
@@ -1,11 +1,11 @@
 package Abstraction;
-import lecture17.*;
+//import lecture17.*;
 public class Hello {
 
 	public static void main(String[] args) {
 //		Hello h=new Hello1();
 //		h.show();
-		Hello1.show();
+//		Hello1.show();
 		
 	}
 
