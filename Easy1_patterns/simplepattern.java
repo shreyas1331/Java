9868456aef7cfd1ed4cf299df80f6d0207a57f9b
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-05-24 00:00:36 +0530

    Patterns

---
+++ Easy1_patterns/simplepattern.java
@@ -0,0 +1,24 @@
+package Easy1_patterns;
+
+public class simplepattern {
+
+	public static void main(String[] args) {
+		int n=4;//given
+		int rows=1;
+		while(rows<=n) {
+			int colm=1;
+			while(colm<=n) {
+				System.out.print(n-rows+1);//u can use "*" & column
+				colm++;				// and now see the output
+			}
+			System.out.println();
+			rows++;
+		}
+	}
+
+}//use all these to see outputs
+//1. "*"
+//2.colm
+//3.rows
+//4.n-colm+1
+//5.n-rows+1
