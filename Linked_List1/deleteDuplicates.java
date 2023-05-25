05b617ada640548370d01ccc423f860fc342590e
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-05-25 16:53:01 +0530

    Linked List

---
+++ Linked_List1/deleteDuplicates.java
@@ -0,0 +1,41 @@
+package Linked_List1;
+
+public class deleteDuplicates {
+
+	public static void main(String[] args) {
+//question-83
+//		
+//		public class Solution {
+//		    public ListNode deleteDuplicates(ListNode head) {
+//		        ListNode temp = head;         
+//		        while(temp != null) {
+//		            if (temp.next == null) break; 
+//		            if (temp.val == temp.next.val) temp.next = temp.next.next;
+//		            else temp = temp.next;
+//		        }         
+//		        return head;
+//		    }
+//		}
+//
+//		----------------------------------------------------------------------------
+//question-203
+//		
+//		class Solution {
+//		    public ListNode removeElements(ListNode head, int val) {
+//		        ListNode result=new ListNode(0);//new linked list
+//		        result.next=head;//next ko head bna diya 
+//		        ListNode temp=result;//head ko ptr de diya 
+//		        while(temp.next!=null)
+//		        {
+//		            if(temp.next.val==val)// to check val
+//		            {
+//		                temp.next=temp.next.next;
+//		            }
+//		            else temp=temp.next;
+//		        }
+//		        return result.next;
+//		    }
+//		}
+	}
+
+}
