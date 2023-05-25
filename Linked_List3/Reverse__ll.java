25b7fdf75d71b6293c3dee26f1263535ba8a37bf
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-05-26 00:31:48 +0530

    Linked List

---
+++ Linked_List3/Reverse__ll.java
@@ -0,0 +1,24 @@
+package Linked_List3;
+
+public class Reverse__ll {
+
+	public static void main(String[] args) {
+//		class Solution {
+//		    public ListNode reverseList(ListNode head) {
+//		        //1 2 3 4 5
+//		        ListNode cur=head;
+//		        ListNode pre=null;
+//		        ListNode after=null;
+//		        while(cur!=null)
+//		        {
+//		            after=cur.next;
+//		            cur.next=pre;
+//		            pre=cur;
+//		            cur=after;
+//		        }
+//		        return pre;
+//		    }
+//		}
+	}
+
+}
