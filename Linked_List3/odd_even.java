31e3b4a952cadb539b5dd27420bacbfa22d6eef1
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-05-29 23:05:07 +0530

    Linked List

--- Linked_List3/odd_even.java
+++ Linked_List3/odd_even.java
@@ -1,5 +1,5 @@
 package Linked_List3;
-
+import java.util.*;
 public class odd_even {
 
 	public static void main(String[] args) {
@@ -19,7 +19,35 @@ public class odd_even {
 //		        return head;
 //		    }
 //		}
+				Scanner sc=new Scanner(System.in);
+				int n=sc.nextInt();//5
+				LinkedList<Integer> list=new LinkedList<Integer>();//linked list bnai
+				for(int i=0;i<n;i++){//inputing in ll
+					int n1=sc.nextInt();
+					list.add(n1);
+				}
+				LinkedList<Integer> l2=new LinkedList<Integer>();//2nd ll
+				for(int i=0;i<n;i++){
+					int n2=list.get(i);
+					if(n2 %2 !=0){//for odd
+						l2.add(n2);
+					}
 
+				}
+				System.out.println(l2);
+				for(int i=0;i<n;i++){
+					int n3=list.get(i);
+					if(n3 %2==0){
+						l2.add(n3);
+					}
+				}
+				System.out.println(l2);
+				for(int i=0;i<n;i++){
+					System.out.print(l2.get(i)+" ");
+				}
+				
+				}
+		
 	}
 
-}
+
