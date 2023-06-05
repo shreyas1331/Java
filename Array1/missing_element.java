435026cdd8248e72f9ae1b8290a46bd1b5672e21
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-06-05 23:18:39 +0530

    Arrays

--- Array1/missing_element.java
+++ Array1/missing_element.java
@@ -2,20 +2,25 @@ package Array1;
 import java.util.*;
 class missing_element {
 
-	public static int missingnumber(int[] a,int N) {
-		
-	}
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int[] arr=new int[n];
-		for(int i=0;i<n;i++) {
+		int sum1=0;
+		for(int i=0;i<n-1;i++) {
 			arr[i]=sc.nextInt();
+			sum1=sum1+arr[i];
 		}
-		for(int i=0;i<n;i++) {
-			System.out.print(arr[i]+" ");
+		int sum=0;
+		for(int i=1;i<=n;i++) {
+			sum=sum+i;
 		}
-		
+		System.out.println(sum);
+		System.out.println(sum1);
+
+		int sum2=sum-sum1;
+		System.out.println(sum2);
+
 	
 		
 	}
