b4c91a3130370012e894becad56cf6d7335a3e63
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-06-05 23:06:02 +0530

    Arrays

---
+++ Array1/missing_element.java
@@ -0,0 +1,22 @@
+package Array1;
+import java.util.*;
+class missing_element {
+
+	public static int missingnumber(int[] a,int N) {
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+	
+		
+	}
+}
