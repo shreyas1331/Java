73ac8d39159fd2b9f9cfd0ce9c27b377e411fe45
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-06-06 23:41:45 +0530

    Arrays

--- Array1/Pair_sum.java
+++ Array1/Pair_sum.java
@@ -19,18 +19,18 @@ public class Pair_sum {
 		return ans;  
 	}
 	
-	static int pairSum(int[] arr,int target) {
-		int n=arr.length;
-		int ans=0;
-		for(int i=0;i<n;i++) {//first number
-			for(int j=i+1;j<n;j++) {//second number
-				if(arr[i]+arr[j]==target) {
-					ans++;
-				}
-			}
-		}
-		return ans;  
-	}
+//	static int pairSum(int[] arr,int target) {
+//		int n=arr.length;
+//		int ans=0;
+//		for(int i=0;i<n;i++) {//first number
+//			for(int j=i+1;j<n;j++) {//second number
+//				if(arr[i]+arr[j]==target) {
+//					ans++;
+//				}
+//			}
+//		}
+//		return ans;  
+//	}
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("enter array size:");
@@ -42,7 +42,7 @@ public class Pair_sum {
 		}
 		System.out.println("enter target sum");
 		int target=sc.nextInt();
-		System.out.println("pair sum is:"+pairSum(arr,target));
+//		System.out.println("pair sum is:"+pairSum(arr,target));
 		System.out.println("triplet sum is:"+tripletSum(arr,target));
 
 	}
---
+++ Array1/Square_of_each.java
@@ -0,0 +1,31 @@
+package Array1;
+import java.util.*;
+public class Square_of_each {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+		arr[i] = sc.nextInt();
+		}
+		Square(arr);
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+		System.out.print(arr[i] + " ");
+		}
+		
+}
+	
+		public static void Square(int arr[]) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+		sum = arr[i] * arr[i];
+		arr[i] = sum;
+		
+		}
+		}
+		}
+	
+
+
