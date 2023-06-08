bc483c2bdfca6a94c70b271fada048f291fd8703
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-06-09 01:25:29 +0530

    Arrays

--- Array1/Square_of_each.java
+++ Array1/Square_of_each.java
@@ -2,6 +2,13 @@ package Array1;
 import java.util.*;
 public class Square_of_each {
 
+	public static void Square(int arr[]) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+		sum = arr[i] * arr[i];
+		arr[i] = sum;
+		}		
+	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
@@ -14,17 +21,7 @@ public class Square_of_each {
 		for (int i = 0; i < arr.length; i++) {
 		System.out.print(arr[i] + " ");
 		}
-		
-}
-	
-		public static void Square(int arr[]) {
-		int sum = 0;
-		for (int i = 0; i < arr.length; i++) {
-		sum = arr[i] * arr[i];
-		arr[i] = sum;
-		
-		}
-		}
+}	
 		}
 	
 
