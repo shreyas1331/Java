6aaf62b71c0d5ce28ee00989dfd7c9d3d525b6d0
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-06-10 10:46:42 +0530

    Hash Maps

--- Hash_Map1/demo1.java
+++ Hash_Map1/demo1.java
@@ -3,24 +3,26 @@ import java.util.*;
 public class demo1 {
 
 	public static void main(String[] args) {
-		HashMap<Integer, String> languages = new HashMap<>();
+		HashMap<Integer, String> hm = new HashMap<>();
 
-	    languages.put(1, "Java");
-	    languages.put(2, "Python");
-	    languages.put(3, "JavaScript");
-	    System.out.println("HashMap: " + languages);
+	    hm.put(1, "Java");
+	    hm.put(2, "Python");
+	    hm.put(3, "JavaScript");
+	    System.out.println("HashMap: " + hm);
 
 	    // return set view of keys
 	    // using keySet()
-	    System.out.println("Keys: " + languages.keySet());
+	    System.out.println("Keys: " + hm.keySet());
 
 	    // return set view of values
 	    // using values()
-	    System.out.println("Values: " + languages.values());
+	    System.out.println("Values: " + hm.values());
 
 	    // return set view of key/value pairs
 	    // using entrySet()
-	    System.out.println("Key/Value mappings: " + languages.entrySet());
+	    System.out.println("Key/Value mappings: " + hm.entrySet());
+	    String value=hm.remove(2);
+	    System.out.println(hm);
 	}
 
 }
