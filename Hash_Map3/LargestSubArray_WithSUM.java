637404abfd6b16d89255d0a597f2a4dd1a549507
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-06-17 16:08:26 +0530

    Hash Map

---
+++ Hash_Map3/LargestSubArray_WithSUM.java
@@ -0,0 +1,30 @@
+package Hash_Map3;
+import java.util.*;
+public class LargestSubArray_WithSUM {
+
+	public static void main(String[] args) {
+		int[] arr= {5,2,3};
+		int Givensum=5;
+		System.out.println(maxLen(arr, Givensum));
+	}
+	public static int maxLen(int[] arr,int Givensum) {
+		// HashMap to store (sum, index) tuples
+		HashMap<Integer,Integer> mp=new HashMap<>();
+		int sum=0;
+		int maxLen=0;
+		for(int i=0;i<arr.length;i++) {
+			sum=sum+arr[i];					// accumulate sum
+			
+			if(sum==Givensum) maxLen=i+1;			// when subarray starts from index '0'
+			
+			if(!mp.containsKey(sum)) mp.put(sum, i); //add to map if noot present
+			
+//			check if 'sum-k' is present in 'map' or not 
+			if(mp.containsKey(sum-Givensum)) {
+				maxLen=Math.max(maxLen, i-mp.get(sum-Givensum));// update maxLength
+			}
+		}
+		return maxLen;
+	}
+
+}
---
+++ Hash_Map3/Subarray_givenSum.java
@@ -0,0 +1,24 @@
+package Hash_Map3;
+import java.util.*;
+public class Subarray_givenSum {
+
+	public static void main(String[] args) {
+		 int[] arr= {5, 8, 6, 13, 3, -1};
+		 int sum=22;
+		 System.out.println(GivenSum(arr,sum));
+	}
+	public static int GivenSum(int[] arr,int sum) {
+		Set<Integer> h=new HashSet<>();
+		int prefix=0;
+		for(int x:arr) {
+			prefix+=x;
+			if(prefix==sum)
+				return 1;
+			if(h.contains(prefix-sum))
+				return 1;
+			h.add(prefix);
+		}
+		return 0;
+	}
+
+}
