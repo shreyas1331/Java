c4fc5fa19d840852bf76730bd53a1720fa10dac9
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2023-06-17 16:05:22 +0530

    Arrays

---
+++ Array3/Two_Sum.java
@@ -0,0 +1,26 @@
+package Array3;
+
+public class Two_Sum {
+
+	public static void main(String[] args) {
+		int[]arr = {2,7,11,15};
+		int target = 9;
+		System.out.print((result));
+		
+	}
+	public static int[] two_sum(int[]arr,int target) {
+		int[] result=new int[2];
+		for(int i=0;i<arr.length;i++) {
+			for(int j=i+1;j<arr.length;j++) {
+				if(arr[i]+arr[j]==target) {
+					result[0]=i;
+					result[1]=j;
+					return result;
+				}
+				
+			}
+			
+		}
+		return result;
+	}
+}
