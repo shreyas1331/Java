3ab468d8880133b3746effeb16a13131f773c4e9
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2025-07-15 22:13:56 +0530

    gfg series

---
+++ Sorting_techniques/selectionSort.java
@@ -0,0 +1,8 @@
+package Sorting_techniques;
+
+public class selectionSort {
+    public static void main(String[] args) {
+        int[]arr={7,5,9,6,8};
+        
+    }
+}
--- Stack1/BalancedParenthesis.java
+++ Stack1/BalancedParenthesis.java
@@ -3,53 +3,42 @@ package Stack1;
 import java.util.*;
 
 public class BalancedParenthesis {
-	
-	static boolean isBalanced(String str)
-	{
-		Stack<Character> s=new Stack<Character>();
-		for(int i=0;i<str.length();i++) {
-			char x=str.charAt(i);
-			if(x=='(' || x=='{' || x=='[')
+
+	static boolean isBalanced(String str) {
+		Stack<Character> s = new Stack<Character>();
+		for (int i = 0; i < str.length(); i++) {
+			char x = str.charAt(i);
+			if (x == '(' || x == '{' || x == '[')
 				s.push(x);
-			else
-			{
-				if(s.empty())
+			else {
+				if (s.empty())
 					return false;
-				else if(x==')' && s.peek()!='(')
-				{
+				else if (x == ')' && s.peek() != '(') {
 					return false;
-				}
-				else if (x == ']' && s.peek() != '[')
-	                {
-	                    return false;
-	                }
-	            else if (x == '}' && s.peek() != '{')
-	                {
-	                    return false;
-	                }
-				else
+				} else if (x == ']' && s.peek() != '[') {
+					return false;
+				} else if (x == '}' && s.peek() != '{') {
+					return false;
+				} else
 					s.pop();
 			}
 		}
-		return (s.isEmpty()==true);
+		return (s.isEmpty() == true);
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a string with parentheses: ");
+		System.out.println("Enter a string with parentheses: ");
 
-        // Check if there is any input available
-        if (scanner.hasNextLine()) {
-            String s1 = scanner.nextLine();
-            if (isBalanced(s1))
-            {
-                System.out.println("The parentheses in the string are balanced.");
-            }
-            else
-            {
-                System.out.println("The parentheses in the string are not balanced.");
-            }
-        }
+		// Check if there is any input available
+		if (scanner.hasNextLine()) {
+			String s1 = scanner.nextLine();
+			if (isBalanced(s1)) {
+				System.out.println("The parentheses in the string are balanced.");
+			} else {
+				System.out.println("The parentheses in the string are not balanced.");
+			}
+		}
 	}
 
 }
