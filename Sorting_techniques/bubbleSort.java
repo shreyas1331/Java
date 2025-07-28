1c8da00175b4d0075ffcda0825c1fef1ab1beb69
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2025-07-28 23:16:16 +0530

    sorting

--- Sorting_techniques/bubbleSort.java
+++ Sorting_techniques/bubbleSort.java
@@ -1,17 +1,21 @@
 package Sorting_techniques;
+
 import java.util.Arrays;
-public class bubbleSort{
+
+//inplace algo, stable algo
+public class bubbleSort {
     public static void main(String[] args) {
-        int[]arr={4,3,2,5,1};
-        bubbleSort(arr,arr.length);
+        int[] arr = { 4, 3, 2, 5, 1 };
+        bubbleSort(arr, arr.length);
     }
-    public static void bubbleSort(int[]arr,int n){
-        for(int i=0;i<n-1;i++){                //n-1 itrations
-            for(int j=0;j<n-i-1;j++){          //last i elements are already in sorted manner
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j];           //swap
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
+
+    public static void bubbleSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) { // n-1 itrations
+            for (int j = 0; j < n - i - 1; j++) { // last i elements are already in sorted manner
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j]; // swap
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
--- Sorting_techniques/selectionSort.java
+++ Sorting_techniques/selectionSort.java
@@ -1,8 +1,27 @@
 package Sorting_techniques;
 
+import java.util.Arrays;
+
 public class selectionSort {
     public static void main(String[] args) {
-        int[]arr={7,5,9,6,8};
-        
+        int[] arr = { 7, 5, 9, 6, 8 };
+        solve(arr, arr.length);
+    }
+
+    public static void solve(int[] arr, int n) {    //find min and swap
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            if (min_idx != i) {
+                int temp = arr[i];
+                arr[i] = arr[min_idx];
+                arr[min_idx] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
