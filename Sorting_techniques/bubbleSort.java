0afd9333c74fb25085d326dc34333c00a1ab58d9
shreyas1331 <shreyas1331.be21@chitkara.edu.in>
2025-08-15 12:00:46 +0530

    sorting tech

--- Sorting_techniques/bubbleSort.java
+++ Sorting_techniques/bubbleSort.java
@@ -11,8 +11,8 @@ public class bubbleSort {
 
     public static void bubbleSort(int[] arr, int n) {
         for (int i = 0; i < n - 1; i++) { // n-1 itrations
-            for (int j = 0; j < n - i - 1; j++) { // last i elements are already in sorted manner
-                if (arr[j] > arr[j + 1]) {
+            for (int j=0;j<n-i-1;j++) { // last i elements are already in sorted manner
+                if (arr[j]>arr[j + 1]) {
                     int temp = arr[j]; // swap
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
